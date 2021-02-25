be2eeac30ab3b62f8fd786fd4dc17f9abb231e90
Eko Wijaya <ekowijayawindows@gmail.com>
2021-02-25 16:30:37 +0700

    Soal Chiper Text berhasil di jalankan

--- src/com/batch254/logic04/Main.java
+++ src/com/batch254/logic04/Main.java
@@ -22,48 +22,48 @@ public class Main {
             input.nextLine();
 
             switch (question) {
-                case 1:
-                    SolveMeFirst.Resolve();
-                    break;
-//                case 2:
-//                    Exam02.Resolve();
+//                case 1:
+//                    Exam1.Resolve();
 //                    break;
+                case 2:
+                    Exam2.Resolve();
+                    break;
                 case 3:
-                    SimpleArraySum.Resolve();
+                    Exam3.Resolve();
                     break;
-                case 4:
-                    DiagonalDifference.Resolve();
-                    break;
-                case 5:
-                    PlusMinus.Resolve();
-                    break;
-                case 6:
-                    Staircase.Resolve();
-                    break;
-                case 7:
-                    MiniMaxSum.Resolve();
-                    break;
-                case 8:
-                    Exam08.Resolve();
-                    break;
-//                case 9:
-//                    Exam09.Resolve();
+//                case 4:
+//                    DiagonalDifference.Resolve();
 //                    break;
-//                case 10:
-//                    Exam10.Resolve();
+//                case 5:
+//                    PlusMinus.Resolve();
 //                    break;
-                case 11:
-                    Sorting.Resolve();
-                    break;
-                case 12:
-                    Mean.Resolve();
-                    break;
-                 case 13:
-                    Median.Resolve();
-                    break;
-                case 14:
-                    Modus.Resolve();
-                    break;
+//                case 6:
+//                    Staircase.Resolve();
+//                    break;
+//                case 7:
+//                    MiniMaxSum.Resolve();
+//                    break;
+//                case 8:
+//                    Exam08.Resolve();
+//                    break;
+////                case 9:
+////                    Exam09.Resolve();
+////                    break;
+////                case 10:
+////                    Exam10.Resolve();
+////                    break;
+//                case 11:
+//                    Sorting.Resolve();
+//                    break;
+//                case 12:
+//                    Mean.Resolve();
+//                    break;
+//                 case 13:
+//                    Median.Resolve();
+//                    break;
+//                case 14:
+//                    Modus.Resolve();
+//                    break;
                 default:
                     System.out.println("SOAL TIDAK DITEMUKAN");
                     break;
